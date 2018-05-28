189347c09ce6a96a682bace77b2b4b216fd17929
Christian Triebstein <c.triebstein@arcor.de>
2018-05-28 21:12:44 +0200

    added month selection for event calendar

--- src/main/java/de/ect/home/automation/controllers/HomeController.java
+++ src/main/java/de/ect/home/automation/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package de.ect.home.automation.controllers;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,9 +15,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import de.ect.home.automation.calendar.FacebookEventService;
 import de.ect.home.automation.calendar.GoogleCalendarEventService;
+import de.ect.home.automation.calendar.util.EventWrapper;
 
 @Controller
 public class HomeController {
@@ -35,10 +43,12 @@ public class HomeController {
 	@GetMapping("/")
 	public String homePage(Model model) throws IOException {
 		// get events from google calendar
-		GoogleCalendarEventService ces = new GoogleCalendarEventService();
-		Calendar c = Calendar.getInstance();
-		model.addAttribute("events", ces.getCalendarEntries(c.get(Calendar.MONTH), c.get(Calendar.YEAR)));
-
+		Calendar selectedMonth = Calendar.getInstance();
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM");
+		
+		model.addAttribute("selectedMonth", df.format(selectedMonth.getTime()));		
+		model.addAttribute("events", getCalendarEntries(df.format(selectedMonth.getTime())));
+		
 		// only if an app id is set for facebook get those events
 		if (!StringUtils.isEmpty(facebookAppId)) {
 			// connect to facebook if that hasn't already happened
@@ -46,9 +56,30 @@ public class HomeController {
 				return "redirect:/connect/facebook";
 			}
 
-			model.addAttribute("facebookEvents", facebookEventService.getFacebookEvents(facebook, c));
+			model.addAttribute("facebookEvents", facebookEventService.getFacebookEvents(facebook, selectedMonth));
 		}
 	
 		return "home";
+	}
+	
+	@RequestMapping(value = "reloadEventData", method = RequestMethod.GET)
+	public String reloadEventData(@RequestParam("month") String month,  Model model) {
+		model.addAttribute("events", getCalendarEntries(month));
+		
+		return "home::eventList";
+	}
+	
+	private List<EventWrapper> getCalendarEntries(String month) {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM");
+		Calendar selectedMonth = Calendar.getInstance();
+		try {
+			selectedMonth.setTime(df.parse(month));
+			GoogleCalendarEventService ces = new GoogleCalendarEventService();
+			
+			return ces.getCalendarEntries(selectedMonth.get(Calendar.MONTH), selectedMonth.get(Calendar.YEAR));
+		}
+		catch (IOException | ParseException e) {
+			return new ArrayList<>();
+		}
 	}
 }
